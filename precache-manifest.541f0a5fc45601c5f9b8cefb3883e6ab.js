1d205cf6657650f1ba0cd969b58c5f75c2403857
Gagandeep Singh <gagandeep16037@iiitd.ac.in>
2019-04-01 01:29:59 +0530

    Updates

---
+++ precache-manifest.541f0a5fc45601c5f9b8cefb3883e6ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "e33601510646ab890937",
+    "url": "/weather-app/static/js/main.ec2d7846.chunk.js"
+  },
+  {
+    "revision": "f160bca266325b6c0409",
+    "url": "/weather-app/static/js/2.e1482529.chunk.js"
+  },
+  {
+    "revision": "e33601510646ab890937",
+    "url": "/weather-app/static/css/main.4108762f.chunk.css"
+  },
+  {
+    "revision": "99c5c08548051ff2c2442dc6d6710f27",
+    "url": "/weather-app/index.html"
+  }
+];
