7ed4698922cce9c3a97f6396b17f2d5cbd07c55f
Gagandeep Singh <gagandeep16037@iiitd.ac.in>
2019-04-01 12:29:29 +0530

    API call fix , http->https

--- src/App.js
+++ src/App.js
@@ -60,9 +60,9 @@ class App extends React.Component {
   getWeather = async (city_name) => {
     // e.preventDefault();
     const city = city_name;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    const api_call2 = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
+    const api_call2 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
     const data2 = await api_call2.json();
     console.log(data2)
   
